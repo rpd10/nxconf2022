1f21b42ef0bfa98a8443cdfd86051ae933ab07cd
Ryan Diehl <rydiehl@cisco.com>
2022-10-02 23:48:53 -0400

    demo: add code that fails lint rule

--- libs/shared/feature/login/src/lib/shared-feature-login.module.ts
+++ libs/shared/feature/login/src/lib/shared-feature-login.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { AdminUiAdminButtonModule } from '@nxconf2022/admin/ui/admin-button';
 import { SharedUiButtonModule } from '@nxconf2022/shared/ui/button';
 import { LoginComponent } from './login/login.component';
 
@@ -9,7 +10,12 @@ const routes: Route[] = [
 ];
 
 @NgModule({
-  imports: [CommonModule, SharedUiButtonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    SharedUiButtonModule,
+    RouterModule.forChild(routes),
+    AdminUiAdminButtonModule,
+  ],
   declarations: [LoginComponent],
   exports: [LoginComponent],
 })
